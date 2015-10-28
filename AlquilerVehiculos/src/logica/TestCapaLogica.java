bd5a9b12724a05cbe6ca88ed956b2a16d90f58fb
hecgode <hecgode@inf.upv.es>
2015-10-28 18:33:48 +0100

    prueba

--- AlquilerVehiculos/src/logica/TestCapaLogica.java
+++ AlquilerVehiculos/src/logica/TestCapaLogica.java
@@ -4,11 +4,10 @@ public class TestCapaLogica {
 
 
 	public  static void main () {
-		AlquilerVehiculos alquiler = new AlquilerVehiculos();
-		
+		AlquilerVehiculos alquiler;
 
 			Reserva reserva1 =new Reserva(1,"12/05/06","12/07/2016","1",cli1,cat1,suc1,suc1);
-			
+
 
 			Reserva reserva2 =new Reserva (2,"13/05/06","25/07/2016",21,cli2,cat2,suc1,suc2);
 			Cliente cli1= new Cliente(111, "Luis", "", "5678", 46018, "14/5/1989", 0, 0, 0, 0, "vISA");
