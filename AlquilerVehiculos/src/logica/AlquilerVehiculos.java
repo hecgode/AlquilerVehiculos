d9d6f42b0cc2f238ebf9b6f905b9b67964030d91
semproms-64 <danubio64@outlook.com>
2015-10-29 08:52:33 +0100

    Prueba_1

--- AlquilerVehiculos/src/logica/AlquilerVehiculos.java
+++ AlquilerVehiculos/src/logica/AlquilerVehiculos.java
@@ -11,6 +11,7 @@ public class AlquilerVehiculos {
     private List<Reserva> mReserva= new ArrayList<Reserva>();
     private List<Categoria> mCategoria = new ArrayList <Categoria>();
     private List<Sucursal> mSucursal = new ArrayList <Sucursal>();
+    private String hola = "Hola mundo";
 
     public AlquilerVehiculos(){
 
