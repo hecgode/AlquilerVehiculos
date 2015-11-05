9898ae58d534ec2eb71419e3514eb7540c5c0e03
hecgode <hecgode@hecgode-Aspire-5737Z>
2015-11-05 10:00:19 +0100

    prueba

---
+++ AlquilerVehiculos/src/persistencia/dto/DanyoDTO.java
@@ -0,0 +1,23 @@
+package persistencia.dto;
+
+public class DanyoDTO {
+	private int id;
+	private String zona;
+	private String descripcion;
+	private int entrega;
+	private int devolucion;
+	
+	public DanyoDTO(int id, String zona, String descripcion, int entrega, int devolucion) {
+		super();
+		this.id = id;
+		this.zona = zona;
+		this.descripcion = descripcion;
+		this.entrega = entrega;
+		this.devolucion = devolucion;
+	}
+	
+	
+	
+	
+
+}
