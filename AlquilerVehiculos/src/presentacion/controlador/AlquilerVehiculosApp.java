e934474537814b4bddc118af5b72572e239f4c67
hecgode <hecgode@hecgode-Aspire-5737Z>
2015-11-12 10:07:06 +0100

    aleluya

---
+++ AlquilerVehiculos/src/presentacion/controlador/AlquilerVehiculosApp.java
@@ -0,0 +1,42 @@
+package presentacion.controlador;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+public class AlquilerVehiculosApp extends Application {
+private Stage primaryStage;
+private BorderPane rootLayout;
+public static void main(String[] args) {
+launch(args);
+}
+public void start(Stage primaryStage) {
+this.primaryStage = primaryStage;
+this.primaryStage.setTitle("ALQUILER DE VEHICULOS");
+initRootLayout();
+}
+public void initRootLayout() {
+try {
+// Load root layout from fxml file.
+FXMLLoader loader = new FXMLLoader();
+loader.setLocation(AlquilerVehiculosApp.class.getResource("presentacion/controlador/principal.fxml"));
+rootLayout = (BorderPane) loader.load();
+// Show the scene containing the root layout.
+Scene scene = new Scene(rootLayout);
+primaryStage.setScene(scene);
+primaryStage.show();
+ControladorPrincipal controlador = loader.getController();
+controlador.setPrimaryStage(primaryStage);
+} catch (IOException e) {
+e.printStackTrace();
+}
+}
+
+}
