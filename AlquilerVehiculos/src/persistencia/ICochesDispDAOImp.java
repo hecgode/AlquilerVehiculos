23d2821d1a516bdec84e0a08f250ece7293dc367
semproms-64 <danubio64@outlook.com>
2015-12-06 12:29:10 +0100

    Probando nuevas consultas.

--- AlquilerVehiculos/src/persistencia/ICochesDispDAOImp.java
+++ AlquilerVehiculos/src/persistencia/ICochesDispDAOImp.java
@@ -25,39 +25,54 @@ public class ICochesDispDAOImp implements ICochesDispDAO {
 			}
 	}
 
-	@Override
 	public List<CochesDispDTO> ObtenerCochesDisponibles(int sucursal, String categoria) throws DAOExcepcion {
+		  try{
+		   int intentos = 1;
+		   List <CochesDispDTO> listaCoches = new ArrayList<CochesDispDTO>();
+		   while(listaCoches.isEmpty() == true && intentos <5)
+		   {
+		   connManager.connect();
+		   ResultSet rs = connManager.queryDB("SELECT * FROM COCHE WHERE SUCURSAL ="+sucursal);
+		   System.out.println("HASTA AQUÃ  BIEN");
+		   if(rs.wasNull())
+		    System.out.println("Consulta vacÃ­a");
+		   connManager.close();
 
-		try{
-			connManager.connect();
-			ResultSet rs = connManager.queryDB("SELECT * FROM COCHE");
-			System.out.println("HASTA AQUÃ� BIEN");
-			if(rs.wasNull())
-				System.out.println("Consulta vacÃ­a");
-			connManager.close();
+		     if(intentos != 1)
+		     {
+		      switch(categoria)
+		      {
+		       case "economy":categoria = "sedán";break;
+		       case "sedán":categoria = "luxury";break;
+		       default:intentos = 5;break;
+		      }
+		     }
+		    try {
 
-			List <CochesDispDTO> listaCoches = new ArrayList<CochesDispDTO>();
+		     while (rs.next()) {
 
-			try {
-
-				while (rs.next()) {
-
-				CochesDispDTO coche = new CochesDispDTO(
-								rs.getString("MATRICULA"),
-								rs.getDouble("KMSACTUALES"),
-								rs.getInt("SUCURSAL"),
-								rs.getString("CATEGORIA"),
-								rs.getString("NOMBRE"));
+		      if(rs.getString("CATEGORIA") == categoria)
+		      {
+		       CochesDispDTO coche = new CochesDispDTO(
+		         rs.getString("MATRICULA"),
+		         rs.getDouble("KMSACTUALES"),
+		         rs.getInt("SUCURSAL"),
+		         rs.getString("CATEGORIA"),
+		         rs.getString("NOMBRE"));
 
 
-							listaCoches.add(coche);
-				}
-					return listaCoches;
+		       listaCoches.add(coche);
+		      }
 
-			} catch (Exception e) { throw new DAOExcepcion(e);}
+		     }
+		    } catch (Exception e) { throw new DAOExcepcion(e);}
+		    intentos++;
+		    }
 
-		} catch (SQLException e) { throw new DAOExcepcion(e);}
-	      catch (DAOExcepcion e) { throw e;}
+		      return listaCoches;
 
-	}
+		  } catch (SQLException e) { throw new DAOExcepcion(e);}
+		       catch (DAOExcepcion e) { throw e;}
+
+		 }
 }
